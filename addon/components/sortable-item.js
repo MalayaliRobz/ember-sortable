5aed2eba40e810f29f84269df5a0f94417c95b57
MalayaliRobz <robinphilipthomas@outlook.in>
2020-11-12 18:30:47 +0530

    convert to native class

--- addon/components/sortable-item.js
+++ addon/components/sortable-item.js
@@ -1,14 +1,13 @@
-import Component from '@ember/component';
-import layout from './template';
+import Component from '@glimmer/component';
 import { bind } from '@ember/runloop';
-import { get, set, setProperties, computed } from '@ember/object';
-import { inject } from '@ember/service';
+import { get, set, setProperties, computed, action } from '@ember/object';
+import { inject as service } from '@ember/service';
 import { isEqual, isEmpty, isPresent } from '@ember/utils';
 import { reads } from '@ember/object/computed';
 import { assert } from '@ember/debug';
 import { assign } from '@ember/polyfills';
-import SortableContainer from '../../classes/sortable-container';
-import ScrollContainer from '../../classes/scroll-container';
+import SortableContainer from '../classes/sortable-container';
+import ScrollContainer from '../classes/scroll-container';
 
 const DRAGACTIONS = ['mousemove', 'touchmove'];
 const DROPACTIONS = ['mouseup', 'touchend'];
@@ -16,33 +15,47 @@ const CONTEXTMENUKEYCODE = 2;
 const PLACEHOLDER_BG_COLOR = '#ccc';
 const SCROLL_ANIMATION_ID = '_dndContainmentScroll';
 
-export default Component.extend({
-  layout,
-  classNames: ['sortable'],
-  attributeBindings: ['position', 'sortable', 'isDisabled:disabled'],
-  classNameBindings: ['isDragingOver:dragging-over', 'isDisabled:disabled'],
-  sortable: true,
-  isDisabled: false,
-  sortManager: inject(),
-  currentSortPane: reads('sortManager.activeSortPane'),
-  activeSortPaneElement: reads('sortManager.activeSortPaneElement'),
-  sourceIndex: reads('sortManager.sourceIndex'),
-  targetIndex: reads('sortManager.targetIndex'),
-  sortableContainer: reads('sortManager.sortableContainer'),
+export default class SortableItemComponent extends Component {
 
-  isDragingOver: computed('sortManager.currentOverItem', function() {
+  // classNames: ['sortable'],
+  // attributeBindings: ['position', 'sortable', 'isDisabled:disabled'],
+  // classNameBindings: ['isDragingOver:dragging-over', 'isDisabled:disabled'],
+  sortable = true
+  isDisabled = false
+	
+	@service sortManager;
+
+	@reads('sortManager.activeSortPane')
+	currentSortPane
+	
+	@reads('sortManager.activeSortPaneElement')
+	activeSortPaneElement
+	
+	@reads('sortManager.sourceIndex')
+	sourceIndex
+	
+	@reads('sortManager.targetIndex')
+  targetIndex
+	
+	@reads('sortManager.sortableContainer')
+	sortableContainer
+
+	@computed('sortManager.currentOverItem')
+  get isDragingOver() {
     let currentOverItem = get(this, 'sortManager.currentOverItem');
 
     return isEqual(currentOverItem, this);
-  }),
+  }
 
-  documentWindow: computed(function() {
+	@computed
+  get documentWindow() {
     return document.querySelector('body');
-  }),
+  }
 
-  containmentContainer: computed('containment', function() {
-    if (this.containment) {
-      let containmentElement = this.element.closest(`${this.containment}`);
+	@computed('containment')
+  get containmentContainer() {
+    if (this.args.containment) {
+      let containmentElement = this.element.closest(`${this.args.containment}`);
 
       return new ScrollContainer(containmentElement, {
         containment: true,
@@ -51,17 +64,10 @@ export default Component.extend({
     }
 
     return null;
-  }),
+  }
 
-  // sortableContainer: computed(function() {
-  //   let element = get(this, 'activeSortPaneElement') || get(this, 'element');
-  //   let scrollContainer = element.closest(`${get(this, 'scrollPane')}`);
-
-  //   return new ScrollContainer(scrollContainer);
-  // }),
-
-  init() {
-    this._super(...arguments);
+	constructor() {
+    super(...arguments);
 
     assert('tagName should not be empty', isEmpty(this.tagName));
 
@@ -72,35 +78,31 @@ export default Component.extend({
     this._onMouseDown = bind(this, this._onMouseDown);
     this._tearDownDragEvents = bind(this, this._tearDownDragEvents);
     this._detachDragEventManager = bind(this, this._detachDragEventManager);
-  },
+  }
 
-  didInsertElement() {
-    this._super(...arguments);
-
-    let element = this.element
+	@action
+	registerListener(element) {
+    this.element = element;
 
     // element.style['touch-action'] = 'none';
 
     element.addEventListener('mousedown', this._onMouseDown);
     element.addEventListener('dragOver', this._onDragover);
-  },
+  }
 
-  willDestroyElement() {
-    this._super(...arguments);
-
-    let element = this.element
-
+	@action
+  unregisterListener(element) {
     element.removeEventListener('mousedown', this._onMouseDown);
     element.removeEventListener('dragOver', this._onDragover);
 
     this._detachDragEventManager();
-  },
+  }
 
   _preventDefaultBehavior(ev) {
     ev.preventDefault();
     ev.stopPropagation();
     ev.stopImmediatePropagation();
-  },
+  }
 
   _onMouseDown(ev) {
     if (isEqual(ev.button, CONTEXTMENUKEYCODE) || this.isDisabled) {
@@ -118,31 +120,31 @@ export default Component.extend({
 
     DRAGACTIONS.forEach(event => window.addEventListener(event, this._dragEventsManager));
     DROPACTIONS.forEach(event => window.addEventListener(event, this._detachDragEventManager));
-  },
+  }
 
   _dragEventsManager(ev) {
     // this._preventDefaultBehavior(ev);
     this._detachDragEventManager();
 
-    this._cloneDraggable();
+		this._cloneDraggable();
 
-    this.sendAction('dragstart', ev);
+    this.args.dragstart ? this.args.dragstart(ev) : '';
 
     DRAGACTIONS.forEach(event => window.addEventListener(event, this._onDrag));
     DROPACTIONS.forEach(event => window.addEventListener(event, this._tearDownDragEvents));
-  },
+  }
 
   _detachDragEventManager() {
     DRAGACTIONS.forEach(event => window.removeEventListener(event, this._dragEventsManager));
     DROPACTIONS.forEach(event => window.removeEventListener(event, this._detachDragEventManager));
-  },
+  }
 
   _tearDownDragEvents() {
     DRAGACTIONS.forEach(event => window.removeEventListener(event, this._onDrag));
     DROPACTIONS.forEach(event => window.removeEventListener(event, this._tearDownDragEvents));
 
     this._onDrop();
-  },
+  }
 
   _cloneDraggable() {
     let sortableContainer = new SortableContainer(this.element);
@@ -165,7 +167,7 @@ export default Component.extend({
     });
 
     sortableContainer.startDrag();
-  },
+  }
 
   getPlaceholderStyles(sortableContainer) {
     return {
@@ -176,7 +178,7 @@ export default Component.extend({
       'border-width': sortableContainer.computedDOMStyles.borderWidth,
       'margin': sortableContainer.computedDOMStyles.margin
     };
-  },
+  }
 
   _onDrag(ev) {
     this._preventDefaultBehavior(ev);
@@ -236,10 +238,10 @@ export default Component.extend({
     if (containmentContainer) {
       containmentContainer.handleScroll(sortableContainer);
     }
-  },
+  }
 
   _onDrop() {
-    this.sendAction('dragend');
+    this.args.dragend ? this.args.dragend() : '';
 
     this.documentWindow.classList.remove('sortable-attached');
     this.documentWindow.removeChild(this.sortableContainer.cloneNode);
@@ -247,9 +249,9 @@ export default Component.extend({
     this.sortableContainer.stopDrag();
 
     if(this.currentSortPane) {
-      this.currentSortPane.send('onDrop', this.element);
+      this.currentSortPane.onDrop ? this.currentSortPane.onDrop(this.element) : '';
     }
-  },
+  }
 
   _onDragover(ev) {
     if (get(this, 'sortManager.isDragging')) {
@@ -275,9 +277,9 @@ export default Component.extend({
         targetIndex
       });
 
-      this.sendAction('onDragover');
+      this.args.dragover ? this.args.dragover() : '';
     }
-  },
+  }
 
   _createEvent(eventType = 'MyCustomEvent', options = {}) {
     let defaults = {
@@ -290,4 +292,5 @@ export default Component.extend({
 
     return new CustomEvent(eventType, mergedOptions);
   }
-});
+
+}
